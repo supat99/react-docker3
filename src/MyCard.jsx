a93099d70b7bbedb1ef20a0f9ebc5af6c98af020
supat9 <bebleforwork@gmail.com>
2024-10-13 12:59:39 +0700

    EditMyCardFile

--- src/MyCard.jsx
+++ src/MyCard.jsx
@@ -27,6 +27,8 @@ export default function MyCard(props) {
   </CardContent>
   <CardActions>
     <Button size="small">Learn More</Button>
+    <Button size="small">BBB</Button>
+
   </CardActions>
 </Card>
 
